be8330db5cd1ad56c4a1ae68863a91a64fa9011a
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-08-09 20:53:49 +0300

    reordered strings in commit

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,7 @@
-// * 3a793ec - (HEAD -> master) initial (68 minutes ago) <Kostiantyn Palchyk>
 const React = require('react');
-const { Text, useInput, Box, Spacer, measureElement } = require('ink');
-const { useState, useRef, useEffect, useMemo } = require('react');
+const { Text, useInput, Box, Spacer } = require('ink');
+const { useState, useMemo } = require('react');
 const { DateTime } = require('luxon');
-const { default: Spinner } = require('ink-spinner');
 
 function Main({ log }) {
   const sizes = { width: '100%', height: 40 };
@@ -22,6 +20,8 @@ function Main({ log }) {
     }
   });
 
+  // * 3a793ec - (HEAD -> master) initial (68 minutes ago) <Kostiantyn Palchyk>
+
   return <Box flexDirection={"column"} height={sizes.height} width={sizes.width}>
     <Box flexGrow={1} flexDirection={"column"} padding={1}>
       {
@@ -31,11 +31,13 @@ function Main({ log }) {
               {'* '}
               <Text color="red">{entry.hash.substring(0, 7)}</Text>
               {' '}
-              <Text color="yellow">{entry.refs}</Text>
+              {entry.refs
+              ? <Text color="yellow">{ entry.refs } </Text>
+              : null
+              }
+              <Text>{entry.message}</Text>
               {' '}
               <Text color="green">({DateTime.fromISO(entry.date).toRelative()})</Text>
-              {' '}
-              <Text>{entry.message}</Text>
               {' '}
               <Text color="blue">&lt;{entry.author_name}&gt;</Text>
             </Text>

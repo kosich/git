7f647ac9f61111a00ebae1c1a063412843d48f3b
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-08-10 12:51:23 +0300

    added message and body to commit details

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 //@ts-check
 const React = require('react');
-const { Text, useInput, Box, Spacer } = require('ink');
+const { Text, useInput, Box, Spacer, Newline } = require('ink');
 
 function Sidebar({ commit }) {
   if (!commit || !commit.diff || !commit.diff.files) {
@@ -10,9 +10,15 @@ function Sidebar({ commit }) {
   const files = commit.diff.files;
 
   return <Box flexDirection="column" padding={1}>
-    <Box>
-      <Text>{ commit.hash }</Text>
-    </Box>
+    <Text color="gray">{ commit.hash }</Text>
+
+    <Newline />
+
+    <Text>{ commit.message }</Text>
+
+    <Newline />
+
+    <Text>{ commit.body }</Text>
 
     <Box padding={1} flexDirection="column">
     {
